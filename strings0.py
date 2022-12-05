cdd3d456ec6d1f798f073cb67562f79755e7cc21
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-12-05 16:41:58 +0530

    Add files via upload

---
+++ strings0.py
@@ -0,0 +1,25 @@
+Python Program for
+# Creation of String
+
+# Creating a String
+# with single Quote
+a = 'pooja'
+print(a)
+
+# Creating a String
+# with double Quotes
+b = "I'm pooja"
+print(b)
+
+# Creating a String
+# with triple Quotes
+c = '''hello this is pooja'''
+print(c)
+
+# Creating String with triple
+# Quotes allows multiple lines
+d = '''Geeks
+			For
+			Life'''
+print(d)
+
